65496cd55a60a1543b8968b326a570566b47622a
gustavo <guthalutha0@gmail.com>
2024-10-11 00:17:17 -0300

    Initial settings

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = new List<Department>();
+            departments.Add(new Department() { Id = Guid.NewGuid(), Name = "Tecnologia" });
+            departments.Add(new Department() { Id = Guid.NewGuid(), Name = "Segurança" });
+
+            return View(departments);
+        }
+    }
+}
---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
